8f7ed002c058453b53cd6d55156df1560c13e2d9
Alan Ruvalcaba <alan@worldnet-intl.com>
2016-06-21 18:51:33 +0000

    Teaser

--- loop.php
+++ loop.php
@@ -1,5 +1,6 @@
 <?php
-for( $i = 0; $i <= 22; $i = $i + 1)
+for( $i = 0, $z =3; $i <= 22; $i = $i + 3, $z += 2)
 {
-    echo $i++ . PHP_EOL;
+    echo sprintf('%d%s',$i+$z,PHP_EOL);
+    echo sprintf('%d%s',--$i+--$z,PHP_EOL);
 }
